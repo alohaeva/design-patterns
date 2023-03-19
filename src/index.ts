4d6229bbeeff77fceb41ea6da2e2bf5dc33f600e
Vladislav Evstigneev <vladislav.e@fora-soft.com>
2023-03-19 18:52:04 +0400

    add common demo logic

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {demoFactory} from "./creationalPatterns/factory/demo";
+import {demoAbstractFactory} from "./creationalPatterns/abstractFactory/demo";
+import {demoBuilder} from "./creationalPatterns/builder/demo";
+import {demoVisitor} from "./behavioralPatterns/visitor/demo";
+
+demoFactory();
+demoAbstractFactory();
+demoBuilder();
+demoVisitor();

0485a4c840458a5cc95202876acc6223fb82f5ad
Vladislav Evstigneev <vladislav.evstigneev@carrier.com>
2024-12-24 11:02:55 +0400

    Add comments

--- src/creationalPatterns/singleton/index.ts
+++ src/creationalPatterns/singleton/index.ts
@@ -1,11 +1,30 @@
 export class Singleton {
+	/**
+	 * Add a private static field for the singleton instance
+	 */
 	private static instance: Singleton;
 
+	/**
+	 * Make the constructor of the class private.
+	 * Only the static method of the class will be able to call the constructor
+	 */
 	private constructor() {}
 
+	/**
+	 * Declare a public static creation method
+	 * Implement “lazy initialization” inside the static method
+	 */
 	public static create() {
-		if (Singleton.instance) return Singleton.instance;
+		if (Singleton.instance) {
+			/**
+			 * The method should always return that instance on all subsequent calls
+			 */
+			return Singleton.instance;
+		}
 
+		/**
+		 * Create a new object on its first call and put it into the static field
+		 */
 		Singleton.instance = new Singleton();
 
 		return Singleton.instance;
